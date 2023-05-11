c258a68a4ba7e17c1e41db435640ac9214e5a78a
finnasaraswati <133173792+finnasaraswati@users.noreply.github.com>
2023-05-11 08:03:53 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,92 @@
+<?php session_start()?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Submission Deploy Aplikasi Profil</title>
+    <link rel="stylesheet" href="cssprofil.css">
+</head>
+
+<body>
+<header>
+    <h1>Finna Diah Saraswati</h1>
+    <p>Undergraduate Student of Information Systems at Universitas Dian Nuswantoro</p>
+</header>
+<nav>
+    <ul>
+      <a href="#profile">Profile</a>
+      <a href="#education">Education</a>
+      <a href="#experience">Experience</a>
+      <a href="#contact">Contact</a>
+    </ul>
+</nav>
+
+<main>
+  <div id="content">
+      <div class="row">
+        <div class="leftcolumn">
+          <div class="card">
+            <h3>About Me</h3>
+              <p>
+                  I'm Finna, an undergraduate student in Information Systems at Universitas Dian Nuswantoro, with a highly motivated individual
+                  in technology, business, and innovation. With a solid educational foundation in Information Systems, I have developed an 
+                  excellent set of interpersonal skills, communication skills, technical skills, and business analyst abilities to assist with 
+                  developing and implementing innovative solutions. I am constantly looking for new challenges and opportunities to broaden my 
+                  knowledge and expertise, and I am dedicated to making a positive impact through my work.
+              </p>
+          </div>
+      
+          <div class="card">
+              <h3>Experience</h3>
+              <h5>Semarang, March - April 2023</h5>
+              <div><img src="assets/diskominfo.jpg"></div>
+              <p>Internship at Diskominfo Kota Semarang</p>
+              <p>
+                Conducting analysis and monitoring of websites for all elementary schools and villages in the city of Semarang as part of a city-wide website competition,
+                and designing the UI/UX for the 2023 Mudik Information and E-Certificate Website for the website competition.
+              </p>
+          </div>
+      </div>
+
+      <div class="rightcolumn">
+        <aside>
+          <article>
+          <div class="sidebar">
+            <h2>Biodata</h2>
+              <figure>
+                  <img src="assets/finna.jpg">
+                    <section>
+                      <table>
+                        <tr>
+                          <th>Nama</th>
+                          <td>: Finna Diah Saraswati</td>
+                        </tr>
+                        <tr>
+                          <th>TTL</th>
+                          <td>: PBun, 2 September 2002</td>
+                        </tr>
+                        <tr>
+                          <th>Path</th>
+                          <td>: Cloud Computing</td>
+                        </tr>
+                        <tr>
+                          <th>Email</th>
+                          <td>:c163dsy0780@bangkit.academy</td>
+                        </tr>
+                      </table>
+                    </section>
+              </figure>
+          </div>
+        </article>
+      </aside>
+  </div>
+</main>
+
+<footer>
+  <h2>Marunting Batu Aji</h2>
+</footer>
+
+</body>
+</html>
